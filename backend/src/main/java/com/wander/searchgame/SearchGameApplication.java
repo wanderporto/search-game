245d8a5d0a19f12d00fd5723c0966c4a045c8444
wander.porto <wanderwang@hotmail.com.br>
2020-09-15 22:17:30 -0300

    subindo projeto base backend

---
+++ backend/src/main/java/com/wander/searchgame/SearchGameApplication.java
@@ -0,0 +1,13 @@
+package com.wander.searchgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchGameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wander/searchgame/SearchGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wander.searchgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
