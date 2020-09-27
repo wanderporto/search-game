77eb8a5342bde263fe5e1631f3a82d96861039b5
wander.porto <wanderwang@hotmail.com.br>
2020-09-27 09:00:35 -0300

    ignore filse

--- .history/backend/src/main/java/com/wander/searchgame/repositories/GameRepository_20200916210411.java
+++
@@ -1,8 +0,0 @@
-package com.wander.searchgame.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface GameRepository extends JpaRepository {
-    
-}
--- .history/backend/src/main/java/com/wander/searchgame/repositories/GameRepository_20200916210457.java
+++
@@ -1,9 +0,0 @@
-package com.wander.searchgame.repositories;
-
-import com.wander.searchgame.entities.Game;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-
-}
